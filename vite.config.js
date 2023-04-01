94267ef6817182dd3e2e1953fc68548cd089eee5
GelacioCode <sal8gelacio@gmail.com>
2023-04-01 22:38:47 +0800

    add build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gelaciocode.github.io/',
+  build: '/gelaciocode.github.io/',
   plugins: [react()],
 })
